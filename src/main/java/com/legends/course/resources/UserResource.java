98e5b8eb8189ddab2e47cb1f616c202c4cff2afb
Valtenis Souza <valtenis.souza.1@gmail.com>
2020-01-11 02:07:54 -0300

    User entity and resource

---
+++ src/main/java/com/legends/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.legends.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.legends.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "valtenis", "valtenis@gmail.com", "123456789", "123");
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
