800ce1d691c57b71ca049da26435c5a79fcce3d3
Valtenis Souza <valtenis.souza.1@gmail.com>
2020-01-14 20:33:29 -0300

    Service layer, component registration

--- src/main/java/com/legends/course/repositories/UserRepository.java
+++ src/main/java/com/legends/course/repositories/UserRepository.java
@@ -5,7 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.legends.course.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
-	
-	 
 
 }
